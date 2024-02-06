01897ce8679cdb7c5f7cf35d5a84747ebe26be0d
Justin Rey A. Ledesma <66949924+justinrey08@users.noreply.github.com>
2024-02-07 00:41:32 +0800

    Week 3 Objects (#13)
    * Added gitignore file
    
    * EDA and File Extraction from S3

---
+++ Automation Scripts/Split_Sales_Data.py
@@ -0,0 +1,48 @@
+# from pyspark.sql import SparkSession
+
+# spark = SparkSession.builder \
+#         .appName('Sales Table') \
+#         .getOrCreate()
+# # path = r'G:\Data Science Job and Internship\Personal Projects\DW-Analytics-Personal-Project\Flat-files\201904 sales reciepts.csv'
+
+# Sales_data = spark.read.csv(path, header = True, inferSchema = True)
+# Sales_data.show()
+
+import boto3
+from cryptography.fernet import Fernet
+import json
+
+# Obtain Creds
+with open("enccreds.json", "r") as json_file:
+    data = json.load(json_file)
+    enc_aws_access_key_id = data['aaki'].encode('ascii')
+    enc_aws_secret_access_key = data['asaki'].encode('ascii')
+
+# decrypt creds
+key = b'vDBN24vpiFChWjZvIJX7kGDeL-NadR4GSNDjn1AltyM='
+fernet = Fernet(key)
+
+aaki_bytes_data = fernet.decrypt(enc_aws_access_key_id)
+asak_bytes_data = fernet.decrypt(enc_aws_secret_access_key)
+aws_access_key_id = aaki_bytes_data.decode('ascii')
+aws_secret_access_key = asak_bytes_data.decode('ascii')
+
+#print(aws_access_key_id)
+#print(aws_secret_access_key)
+
+# Declare necessary arguments for S3 bucket access
+region = 'ap-east-1'
+bucket_name = 'dw-analytics-personal-project'
+file_key = 'dataset/201904 sales reciepts.csv'
+
+
+# Create session
+session = boto3.Session(aws_access_key_id=aws_access_key_id,
+                        aws_secret_access_key=aws_secret_access_key,
+                        region_name=region)
+s3 = session.client('s3')
+s3.download_file(bucket_name, file_key, 'Sales Data.csv')
+
+# print('Download Successful')
+
+# #spark.stop()
