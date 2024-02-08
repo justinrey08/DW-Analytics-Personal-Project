64a9acf69655f1b5b60e6c0c80dd72057f2d46a9
Justin Rey A. Ledesma <66949924+justinrey08@users.noreply.github.com>
2024-02-08 22:42:33 +0800

    Week 3 (#15)
    * Added gitignore file
    
    * EDA and File Extraction from S3
    
    * Added preliminary SQL objects and requirements.txt
    
    * Removed explicit PK declarations

--- PostgreSQL Objects/Tables/dm_product_info.sql
+++ PostgreSQL Objects/Tables/dm_product_info.sql
@@ -1,9 +1,9 @@
 CREATE TABLE csp.dm_product_info (
-    product_id INT REFERENCES csp.fe_product(product_id),
+    product_id INT,
     new_product VARCHAR(50),
     product VARCHAR(50),
     product_category VARCHAR(50),
     product_description VARCHAR(255),
-    prodcut_group VARCHAR(50),
+    product_group VARCHAR(50),
     product_type VARCHAR(50)
 )
--- PostgreSQL Objects/Tables/dm_product_prices.sql
+++ PostgreSQL Objects/Tables/dm_product_prices.sql
@@ -1,5 +1,5 @@
 CREATE TABLE csp.dm_product_prices (
-	product_id INT REFERENCES csp.fe_product(product_id),
+	product_id,
 	current_retail_price FLOAT,
 	current_wholesale_price FLOAT
 )
