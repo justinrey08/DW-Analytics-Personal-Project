fbb3194950e4b6dc00b77935fbc6fb66302e18b6
Justin Rey A. Ledesma <66949924+justinrey08@users.noreply.github.com>
2024-02-08 22:57:09 +0800

    Week 3 (#19)
    * Added gitignore file
    
    * EDA and File Extraction from S3
    
    * Added preliminary SQL objects and requirements.txt
    
    * Removed explicit PK declarations
    
    * Syntax Fix
    
    * Week 3 (#15) (#16)

--- PostgreSQL Objects/Tables/fe_product.sql
+++ PostgreSQL Objects/Tables/fe_product.sql
@@ -1,11 +1,11 @@
 CREATE TABLE csp.fe_product (
-    product_id INT PRIMARY KEY,
+    product_id INT,
     current_retail_price FLOAT,
     current_wholesale_price FLOAT,
     new_product VARCHAR(50),
     product_category VARCHAR(50),
     product_description VARCHAR(255),
-    prodcut_group VARCHAR(50),
+    product_group VARCHAR(50),
     product_type VARCHAR(50),
     promo BOOL,
     tax_exempt BOOL,
