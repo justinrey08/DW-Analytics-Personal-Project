7e44f8965d5124ea510c3be23f4ee8a49c358000
Justin Ledesma <justinreyledesma@gmail.com>
2024-02-12 19:59:59 +0800

    Removed key credentials in files

--- Automation Scripts/Split_Sales_Data.py
+++ Automation Scripts/Split_Sales_Data.py
@@ -11,20 +11,21 @@ import json
 # Obtain Creds
 with open("enccreds.json", "r") as json_file:
     data = json.load(json_file)
+    kilo = data['kilo'].encode('ascii')
     enc_aws_access_key_id = data['aaki'].encode('ascii')
     enc_aws_secret_access_key = data['asaki'].encode('ascii')
 
-# decrypt creds
-key = b'vDBN24vpiFChWjZvIJX7kGDeL-NadR4GSNDjn1AltyM='
-fernet = Fernet(key)
+# decrypt credentials
+fernet = Fernet(kilo)
 
 aaki_bytes_data = fernet.decrypt(enc_aws_access_key_id)
 asak_bytes_data = fernet.decrypt(enc_aws_secret_access_key)
 aws_access_key_id = aaki_bytes_data.decode('ascii')
 aws_secret_access_key = asak_bytes_data.decode('ascii')
 
-#print(aws_access_key_id)
-#print(aws_secret_access_key)
+# print(key)
+# #print(aws_access_key_id)
+# #print(aws_secret_access_key)
 
 # Declare necessary arguments for S3 bucket access
 region = 'ap-east-1'
@@ -37,7 +38,7 @@ session = boto3.Session(aws_access_key_id=aws_access_key_id,
                         aws_secret_access_key=aws_secret_access_key,
                         region_name=region)
 s3 = session.client('s3')
-# s3.download_file(bucket_name, file_key, 'Sales Data.csv')
-# print('Download Successful')
+#s3.download_file(bucket_name, file_key, 'Sales Data.csv')
+#print('Download Successful')
 
 # #spark.stop()
